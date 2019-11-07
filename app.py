0e239fd08c701d27d11883f6059b5884cd9f7bec
Dylan Cooper <genejockey@gmail.com>
2019-11-07 11:55:07 -0600

    containerize flask app

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hey, we have a Flask application!'
+    return 'Hey, we have a Flask application inside a docker container!'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
