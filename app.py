bc33836599dde0fcf0f485a9cd53d0f753c1457b
Dylan Cooper <genejockey@gmail.com>
2019-11-07 12:27:05 -0600

    parse raw POST data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -6,13 +6,18 @@ test = [
     {'text': u'you POSTed something'}
 ]
 
+
 @app.route('/')
 def index():
     return 'Hey, we have a Flask application inside a docker container!'
 
-@app.route('/text-sim/api/v1.0/test', methods=['POST'])
-def post_test():
-    return jsonify(test)
+
+@app.route('/text-sim/api/v1.0/compare', methods=['POST'])
+def parse_request():
+    request.get_data()
+    data = request.data
+    return data
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
