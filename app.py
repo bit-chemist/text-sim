f08ed0142cc914b85e2f3ea169d2077a360510c2
Dylan Cooper <genejockey@gmail.com>
2019-11-07 12:08:42 -0600

    implement basic api and POST method

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+test = [
+    {'text': u'you POSTed something'}
+]
 
 @app.route('/')
 def index():
     return 'Hey, we have a Flask application inside a docker container!'
 
+@app.route('/text-sim/api/v1.0/test', methods=['POST'])
+def post_test():
+    return jsonify(test)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
